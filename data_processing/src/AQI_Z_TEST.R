bd1241a3875117642a52e653daedd387413ea8a4
Henry Roeth <roethhk@hiram.edu>
2023-06-26 15:18:44 -0400

    data processing update

--- data_processing/src/AQI_Z_TEST.R
+++ data_processing/src/AQI_Z_TEST.R
@@ -1,9 +1,7 @@
 
-
-
 #create table
-data <- matrix(c(120, 90, 40, 110), ncol=2, byrow=TRUE)
-colnames(data) <- c("PM2.5","Ozone")
+data <- matrix(c(90, 90), ncol=1, byrow=TRUE)
+colnames(data) <- c("PM2.5")
 rownames(data) <- c("Air.gov","Field Station")
 data <- as.table(data)
 
@@ -13,3 +11,14 @@ data
 #Perform Chi-Square Test of Independence
 chisq.test(data)
 
+#create table
+data <- matrix(c(120, 90), ncol=1, byrow=TRUE)
+colnames(data) <- c("Ozone")
+rownames(data) <- c("Air.gov","Field Station")
+data <- as.table(data)
+
+data
+
+
+#Perform Chi-Square Test of Independence
+chisq.test(data)
