8703beab94c266bafc7b641f055df1e0ecb66e88
Henry Roeth <roethhk@hiram.edu>
2023-06-30 11:38:40 -0400

    changes

--- data_processing/src/csv_file_grapher.py
+++ data_processing/src/csv_file_grapher.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from pandas import *
  
 # reading CSV file
-data = read_csv('2023-06-29&30.csv')
+data = read_csv('2023-06-28&29.csv')
 
 # converting column data to list
 time = data['Time'].tolist()
@@ -27,14 +27,15 @@ x1 = time[1:len(time):60]
 y1 = pm_readings[1:len(pm_readings):60]
 
 
-# plotting the line 1 points 
-plt.plot(x1, y1, label = "PM2.5(ug/m3)")
 
+# plotting the line 1 points 
+plt.plot(x1, y1, color = 'cyan', label = "PM2.5(ug/m3)")
+plt.grid()
 # line 2 points
 x2 = time[1:len(time):60]
 y2 = wind_readings[1:len(wind_readings):60]
 # plotting the line 2 points 
-plt.plot(x2, y2, label = "Wind Speed(mph)")
+plt.plot(x2, y2, color = 'violet', label = "Wind Speed(mph)")
 
 # x3 = time[1:len(time):60]
 # y3 = wind_directions[1:len(wind_directions):60]
@@ -51,6 +52,7 @@ ax = plt.gca()
 ax.set_xticklabels(ax.get_xticks(), rotation = 45)
 ax.set_yticklabels(ax.get_yticks(), rotation = 0)
 # show a legend on the plot
+
 plt.legend()
   
 # function to show the plot
