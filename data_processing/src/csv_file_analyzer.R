649519071f3f2382fb96efbde8d6b9a3805d33da
Henry Roeth <henrykroeth@gmail.com>
2023-07-01 17:33:48 -0400

    sinusoidal data presentations

--- data_processing/src/csv_file_analyzer.R
+++ data_processing/src/csv_file_analyzer.R
@@ -1,5 +1,5 @@
 # Reads CSV file 
-read_csv = read.csv("C:\\Users\\henry\\fs_sample_testing\\data_records\\2023-06-29&30.CSV")
+read_csv = read.csv("C:\\Users\\henry\\fs_sample_testing\\data_records\\2023-06-28&29.CSV")
 
 #Where column name is stored
 col_name=read_csv$Pm2.5.ug.m3.
@@ -16,8 +16,7 @@ print(result <- sd(col_name))
 print(result <- median(col_name))
 
 #Plots the data into a visual display by converting file data into a matrix
-plot(x <- data.frame(col_name2), y <- data.matrix(col_name))
-
+plot(x <- data.matrix(col_name2), y <- data.matrix(col_name), xlab = 'Wind(mph)', ylab = 'Pm2.5(ug/m3)', main = 'Wind & Pm2.5 Sinusoidal Regression Curves 2023-06-(10am)29to(10am)30')
 df <- data.frame(x=data.matrix(col_name2),
                  y=data.matrix(col_name))
 print(fit1 <- lm(y~x, data=df))
@@ -26,7 +25,7 @@ fit3 <- lm(y~poly(x,3,raw=TRUE), data=df)
 fit4 <- lm(y~poly(x,4,raw=TRUE), data=df)
 fit5 <- lm(y~poly(x,5,raw=TRUE), data=df)
 
-x_axis <- seq(0, 8, length=2000)
+x_axis <- seq(0, 9, length=2000)
 
 lines(x_axis, predict(fit1, data.frame(x=x_axis)), col='green')
 lines(x_axis, predict(fit2, data.frame(x=x_axis)), col='red')
