4444d55df6fa8475fd0423246401b601a72ab2a7
Henry Roeth <henrykroeth@gmail.com>
2023-07-01 17:15:54 -0400

    sinusoidal regression function added to R script

--- data_processing/src/csv_file_analyzer.R
+++ data_processing/src/csv_file_analyzer.R
@@ -1,6 +1,35 @@
-# Read CSV into DataFrame
-read_csv = read.csv("C:\\Users\\henry\\fs_sample_testing\\data_records\\2023-06-28&29.CSV")
-print(result <- mean(read_csv$Pm2.5.ug.m3.))
-print(result <- sd(read_csv$Pm2.5.ug.m3.))
-print(result <- median(read_csv$Pm2.5.ug.m3.))
+# Reads CSV file 
+read_csv = read.csv("C:\\Users\\henry\\fs_sample_testing\\data_records\\2023-06-29&30.CSV")
 
+#Where column name is stored
+col_name=read_csv$Pm2.5.ug.m3.
+col_name1=read_csv$Time
+col_name2=read_csv$Wind.mph.
+
+#Find the mean the specified column
+print(result <- mean(col_name))
+
+#Find the standard deviation the specified column
+print(result <- sd(col_name))
+
+#Find the median the specified column
+print(result <- median(col_name))
+
+#Plots the data into a visual display by converting file data into a matrix
+plot(x <- data.frame(col_name2), y <- data.matrix(col_name))
+
+df <- data.frame(x=data.matrix(col_name2),
+                 y=data.matrix(col_name))
+print(fit1 <- lm(y~x, data=df))
+fit2 <- lm(y~poly(x,2,raw=TRUE), data=df)
+fit3 <- lm(y~poly(x,3,raw=TRUE), data=df)
+fit4 <- lm(y~poly(x,4,raw=TRUE), data=df)
+fit5 <- lm(y~poly(x,5,raw=TRUE), data=df)
+
+x_axis <- seq(0, 8, length=2000)
+
+lines(x_axis, predict(fit1, data.frame(x=x_axis)), col='green')
+lines(x_axis, predict(fit2, data.frame(x=x_axis)), col='red')
+lines(x_axis, predict(fit3, data.frame(x=x_axis)), col='purple')
+lines(x_axis, predict(fit4, data.frame(x=x_axis)), col='blue')
+lines(x_axis, predict(fit5, data.frame(x=x_axis)), col='orange')
