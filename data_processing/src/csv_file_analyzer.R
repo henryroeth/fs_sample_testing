97b20c61499750d498b02c9011500b5d377023d3
Henry Roeth <henrykroeth@gmail.com>
2023-07-01 14:12:39 -0400

    R file changes

--- data_processing/src/csv_file_analyzer.R
+++ data_processing/src/csv_file_analyzer.R
@@ -1,5 +1,6 @@
 # Read CSV into DataFrame
-read_csv = read.csv("C:\\Users\\henry\\fs_sample_testing\\data_records\\2023-06-29&30.CSV")
-# print(read_csv)
-print(result <- mean(read_csv$Indoor.Temperature...))
+read_csv = read.csv("C:\\Users\\henry\\fs_sample_testing\\data_records\\2023-06-28&29.CSV")
+print(result <- mean(read_csv$Pm2.5.ug.m3.))
+print(result <- sd(read_csv$Pm2.5.ug.m3.))
+print(result <- median(read_csv$Pm2.5.ug.m3.))
 
