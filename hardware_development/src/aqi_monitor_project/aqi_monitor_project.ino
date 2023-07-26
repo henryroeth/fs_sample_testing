abbc0d1a5878d53b626f7615e1d138101aef60ab
Henry Roeth <roethhk@hiram.edu>
2023-07-26 10:07:11 -0400

    updates

--- hardware_development/src/aqi_monitor_project/aqi_monitor_project.ino
+++ hardware_development/src/aqi_monitor_project/aqi_monitor_project.ino
@@ -2,7 +2,9 @@
 #include "MHZ19.h"                                         // include main library
 #include <SoftwareSerial.h> 
 #include <SPI.h>
-#include <SD.h>                               // Remove if using HardwareSerial or non-uno library compatable device
+#include <SD.h>   
+#include <MQ131.h>  
+// #include <C401_Steam_Air_MQ131_O3.h>                      // Remove if using HardwareSerial or non-uno library compatable device
 
 #define RX_PIN 2                                          // Rx pin which the MHZ19 Tx pin is attached to
 #define TX_PIN 3                                          // Tx pin which the MHZ19 Rx pin is attached to
@@ -31,29 +33,36 @@ void setup()
     mySerial.begin(BAUDRATE);                               // Uno example: Begin Stream with MHZ19 baudrate
     myMHZ19.begin(mySerial);                                // *Important, Pass your Stream reference 
     myMHZ19.autoCalibration(); 
-    dataFile = SD.open("AQI_data.txt", FILE_WRITE); 
-    dataFile.println("Elapsed Time(seconds),CO2(ppm)");
+    MQ131.begin(4, A0, LOW_CONCENTRATION,1000000);
+    MQ131.calibrate();
+    dataFile = SD.open("AQI_data.csv", FILE_WRITE); 
+    dataFile.println("Elapsed Time(minutes),CO2(ppm),Ozone(ppm)");
     dataFile.close();                            // Turn auto calibration ON (disable with autoCalibration(false))
 }
 
 uint16_t line = 1;
 void loop(){
   int CO2; 
-  dataFile = SD.open("AQI_data.txt", FILE_WRITE);                                          
+  dataFile = SD.open("AQI_data.csv", FILE_WRITE);                                          
 
   if(dataFile) {
+    delay(60000);
     byte CO2 = myMHZ19.getCO2();                             
     Serial.print("CO2: ");                      
     Serial.print(CO2);   
     Serial.println(" ppm"); 
+    Serial.print("O3: ");
+    Serial.print(MQ131.getO3(PPM));
+    Serial.println(" ppm");
     
     dataFile.print(line++);
     dataFile.print(",");
-    dataFile.println(CO2);
+    dataFile.print(CO2);
+    dataFile.print(",");
+    dataFile.println(MQ131.getO3(PPM));
     dataFile.close();
-
-    delay(1000);   
+ 
   } else {
-    Serial.println("error opening AQI_data.txt");
+    Serial.println("error opening AQI_data.csv");
   }                         
 }
