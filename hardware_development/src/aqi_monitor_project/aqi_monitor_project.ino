b6b2b09eae9d826c4efabebf0addf4d2b8f82e04
Henry Roeth <roethhk@hiram.edu>
2023-07-31 10:37:28 -0400

    updates

--- hardware_development/src/aqi_monitor_project/aqi_monitor_project.ino
+++ hardware_development/src/aqi_monitor_project/aqi_monitor_project.ino
@@ -1,26 +1,13 @@
-#include <Arduino.h>
-#include "MHZ19.h"                                         // include main library
-#include <SoftwareSerial.h> 
+#include "SoftwareSerial.h"
 #include <SPI.h>
-#include <SD.h>   
-#include <MQ131.h>  
-// #include <C401_Steam_Air_MQ131_O3.h>                      // Remove if using HardwareSerial or non-uno library compatable device
-
-#define RX_PIN 2                                          // Rx pin which the MHZ19 Tx pin is attached to
-#define TX_PIN 3                                          // Tx pin which the MHZ19 Rx pin is attached to
-#define BAUDRATE 9600                                      // Native to the sensor (do not change)
-
-File dataFile;
-MHZ19 myMHZ19;                                             // Constructor for MH-Z19 class
-SoftwareSerial mySerial(RX_PIN, TX_PIN);                   // Uno example
-//HardwareSerial mySerial(1);                              // ESP32 Example
-
-                            // Variable to store timer interval
-
+#include <SD.h> 
+ 
+SoftwareSerial pmsSerial(2, 3); // RX, TX
+File dataFile;  
+ 
 void setup()
 {
-  Serial.begin(115200);
-  pmsSerial.begin(9600);
+  Serial.begin(9600);
   while (!Serial)
   ; // wait for serial port to connect. Needed for native USB port only
   Serial.print("Initializing SD card...");
@@ -30,35 +17,88 @@ void setup()
     while (1);
   }
   Serial.println("initialization done.");
-  delay(2000);                                  // For ESP32 baudarte is 115200 etc. 
-    dataFile = SD.open("AQI_data.csv", FILE_WRITE); 
-    dataFile.println("Elapsed Time(minutes),PM2.5(ug/m3),PM10(ug/m3)");
-    dataFile.close();                            // Turn auto calibration ON (disable with autoCalibration(false))
+  delay(2000);
+  pmsSerial.begin(9600);
+  dataFile = SD.open("AQI_data.csv", FILE_WRITE); 
+  dataFile.println("Seconds,PM2.5(ug/m3),PM10(ug/m3),O3(ppm),NO2(ppm),CO(ppm)");
+  dataFile.close(); 
+}
+ 
+struct pms5003data {
+  uint16_t framelen;
+  uint16_t pm10_standard, pm25_standard, pm100_standard;
+  uint16_t pm10_env, pm25_env, pm100_env;
+  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
+  uint16_t unused;
+  uint16_t checksum;
+};
+ 
+struct pms5003data data;
+uint16_t line = 1;
+
+void loop()
+{
+  dataFile = SD.open("AQI_data.csv", FILE_WRITE);
+    if (readPMSdata(&pmsSerial)) {
+      Serial.print("PM 1.0: "); Serial.println(data.pm10_standard);
+      Serial.print("PM 2.5: "); Serial.println(data.pm25_standard);
+      Serial.print("PM 10: "); Serial.println(data.pm100_standard);
+
+      dataFile.print(line++);
+      dataFile.print(",");
+      dataFile.print(data.pm25_standard);
+      dataFile.print(",");
+      dataFile.println(data.pm100_standard);
+  }
+  dataFile.close();
 }
 
-uint16_t line = 1;
-void loop(){
-
-  dataFile = SD.open("AQI_data.csv", FILE_WRITE);                                          
-
-  if(dataFile) {
-    delay(60000);
-    byte CO2 = myMHZ19.getCO2();                             
-    Serial.print("CO2: ");                      
-    Serial.print(CO2);   
-    Serial.println(" ppm"); 
-    Serial.print("O3: ");
-    Serial.print(MQ131.getO3(PPM));
-    Serial.println(" ppm");
-    
-    dataFile.print(line++);
-    dataFile.print(",");
-    dataFile.print(CO2);
-    dataFile.print(",");
-    dataFile.println(MQ131.getO3(PPM));
-    dataFile.close();
+boolean readPMSdata(Stream *s) {
+  if (! s->available()) {
+    return false;
+  }
+  
+  // Read a byte at a time until we get to the special '0x42' start-byte
+  if (s->peek() != 0x42) {
+    s->read();
+    return false;
+  }
  
-  } else {
-    Serial.println("error opening AQI_data.csv");
-  }                         
+  // Now read all 32 bytes
+  if (s->available() < 32) {
+    return false;
+  }
+    
+  uint8_t buffer[32];    
+  uint16_t sum = 0;
+  s->readBytes(buffer, 32);
+ 
+  // get checksum ready
+  for (uint8_t i=0; i<30; i++) {
+    sum += buffer[i];
+  }
+ 
+  /* debugging
+  for (uint8_t i=2; i<32; i++) {
+    Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
+  }
+  Serial.println();
+  */
+  
+  // The data comes in endian'd, this solves it so it works on all platforms
+  uint16_t buffer_u16[15];
+  for (uint8_t i=0; i<15; i++) {
+    buffer_u16[i] = buffer[2 + i*2 + 1];
+    buffer_u16[i] += (buffer[2 + i*2] << 8);
+  }
+ 
+  // put it into a nice struct :)
+  memcpy((void *)&data, (void *)buffer_u16, 30);
+ 
+  if (sum != data.checksum) {
+    Serial.println("Checksum failure");
+    return false;
+  }
+  // success!
+  return true;
 }
