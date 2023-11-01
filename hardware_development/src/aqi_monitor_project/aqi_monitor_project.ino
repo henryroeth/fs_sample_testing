96df99346b168a0ec375dc592dc75f9d2f655d3a
Henry Roeth <roethhk@hiram.edu>
2023-11-01 12:18:29 -0400

    changes applied

--- hardware_development/src/aqi_monitor_project/aqi_monitor_project.ino
+++ hardware_development/src/aqi_monitor_project/aqi_monitor_project.ino
@@ -78,7 +78,6 @@ boolean readPMSdata(Stream *s) {
     return false;
   }
  
-  // Now read all 32 bytes
   if (s->available() < 32) {
     return false;
   }
@@ -87,32 +86,21 @@ boolean readPMSdata(Stream *s) {
   uint16_t sum = 0;
   s->readBytes(buffer, 32);
  
-  // get checksum ready
   for (uint8_t i=0; i<30; i++) {
     sum += buffer[i];
   }
- 
-  /* debugging
-  for (uint8_t i=2; i<32; i++) {
-    Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
-  }
-  Serial.println();
-  */
   
-  // The data comes in endian'd, this solves it so it works on all platforms
   uint16_t buffer_u16[15];
   for (uint8_t i=0; i<15; i++) {
     buffer_u16[i] = buffer[2 + i*2 + 1];
     buffer_u16[i] += (buffer[2 + i*2] << 8);
   }
  
-  // put it into a nice struct :)
   memcpy((void *)&data, (void *)buffer_u16, 30);
  
   if (sum != data.checksum) {
     Serial.println("Checksum failure");
     return false;
   }
-  // success!
   return true;
 }
